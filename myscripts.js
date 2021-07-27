70b1393b3a00021916e4ca312f241bc1d1b6c826
Mikhail <mikhailgrigoriev92@gmail.com>
2021-07-27 11:51:19 -0400

    managed to create a grid layout, but need to remove space between gridItems

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector("#gridContainer");
 
-for (i=0; i<=16; i++){
+for (i=0; i<=255; i++){
     let generatedGridItem = document.createElement('div');
     generatedGridItem.classList.add('gridItem');
     gridContainer.appendChild(generatedGridItem);
