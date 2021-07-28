b0fe76fefa25edb282bb5fdae8c0b0bf0f454c91
Mikhail <mikhailgrigoriev92@gmail.com>
2021-07-27 23:00:40 -0400

    Added blue cover on hover using a mouseover addEventListener with a callback function that takes an event as its argument, and uses the event.target to turn the background style to blue

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,13 @@
 const gridContainer = document.querySelector("#gridContainer");
 
+
 for (i=0; i<=255; i++){
     let generatedGridItem = document.createElement('div');
     generatedGridItem.classList.add('gridItem');
     gridContainer.appendChild(generatedGridItem);
+    generatedGridItem.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'blue';
+    
+    })
 }
+
