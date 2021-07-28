a18946cc46dae82151060b1f28b1e9605cba62e7
Mikhail <mikhailgrigoriev92@gmail.com>
2021-07-28 11:39:11 -0400

    began creating calculateQuantityOfGridSquares() function, though it still needs work to return even numbers. furthermore, generateGrid needs more work to take in the values of aforementioned function

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,21 @@
 const clearButton = document.querySelector('#clearButton');
+let userSquarePerRowInput = 0;
+let widthOfGridItem = 0;
+let totalGridItemsInContainer = 0;
+
+function calculateQuantityOfGridSquares(){
+    userSquarePerRowInput = prompt("How many squares (less than 100) would you like one side of your grid to have?");
+    while (userSquarePerRowInput>=100){
+        userSquarePerRowInput = prompt("You accidentally typed in a number over 100, please type in another less than 100!");
+        }
+    widthOfGridItem = 10000 / userSquarePerRowInput;
+    totalGridItemsInContainer = userSquarePerRowInput * userSquarePerRowInput; 
+}
+
+calculateQuantityOfGridSquares();
 
 function generateGrid(gridContainer) {
-    for (i=0; i<=255; i++){
+    for (i=0; i<totalGridItemsInContainer; i++){
         let gridContainer = document.querySelector("#gridContainer");
         let generatedGridItem = document.createElement('div');
         generatedGridItem.classList.add('gridItem');
@@ -12,14 +26,28 @@ function generateGrid(gridContainer) {
     }
 }
 
+ // 10,000 pixels should be the size of the grid container
+
+calculateQuantityOfGridSquares();
+
 generateGrid();
 
 /*
+
+function clearGrid(){
+
+}
+
 clearButton.addEventListener('click', () => {
+});
+    First the button can have the functionality of taking in user input, and generating 
+    the correct number of squares within a gridContainer of 10,000 pixels.
+
+    After that functionality works, I will add the clearGrid function
+
     // first this button sends a prompt asking how many squares per row
     // if the number is above 100 it sends an alert and starts over
-    // we assign gridItem width and height to 160 (pixels per side of 
-    // square) / numberOfSquares to find out width and height of square
+    // we find size of grid item width and height
     // then we run another seperately created function for loop to .removeChild 256 time
     // then we turn the above for loop into a function where 255 is replaced 
     // by a value we can input
@@ -30,4 +58,4 @@ clearButton.addEventListener('click', () => {
     // final functions, place there at the top of the code seperate from the addEventListener
     // : removeGridItems (for loop), createGridItems (for loop), calculateGridItemSize (math)
 
-})*/
+*/
